034f025cd2bae669dfa8a5872378922320bb9f99
leet228 <misha.orlov208@gmail.com>
2025-08-26 02:01:28 +0300

    Update db and cache current topic and subtopic

--- src/components/sheets/CourseSheet.tsx
+++ src/components/sheets/CourseSheet.tsx
@@ -161,7 +161,7 @@ export default function CoursesPanel(props: Props) {
                   <img
                     src={`/subjects/${s.code}.svg`}
                     alt={s.title}
-                    className="w-[112px] h-[112px] object-contain relative -translate-y-5"
+                    className="w-[112px] h-[112px] object-contain relative -translate-y-9"
                     onError={(e) => { (e.currentTarget as HTMLImageElement).style.display = 'none'; }}
                   />
                 </div>
