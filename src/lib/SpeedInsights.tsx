08ed9552f89919484ce54515e330b83055aec915
misha <misha.orlov208@gmail.com>
2025-09-02 00:10:23 +0300

    Update vercel analytics

--- src/lib/SpeedInsights.tsx
+++ src/lib/SpeedInsights.tsx
@@ -1,9 +1,16 @@
 import { useLocation } from 'react-router-dom';
 import { SpeedInsights as VSISpeedInsights } from '@vercel/speed-insights/react';
+// @ts-ignore: package installed at deploy; suppress type resolution in local env
+import { Analytics as VercelAnalytics } from '@vercel/analytics/react';
 
 export default function SpeedInsights() {
   const { pathname } = useLocation();
-  return <VSISpeedInsights route={pathname} />;
+  return (
+    <>
+      <VSISpeedInsights route={pathname} />
+      <VercelAnalytics />
+    </>
+  );
 }
 
 
